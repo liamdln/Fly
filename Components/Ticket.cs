357fe1706b98d2615fe63812a9b04f31b14d69f5
liamdp <liam.pickering@outlook.com>
2020-10-14 14:22:59 +0100

    made changes to BookingListForm

--- Components/Ticket.cs
+++ Components/Ticket.cs
@@ -4,22 +4,22 @@
     {
         public Ticket(int ticketID, string firstName, string lastName, string hashedCardNumber, bool collected, int flightID, int bookedSeats)
         {
-            this.ticketID = ticketID;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.hashedCardNumber = hashedCardNumber;
-            this.collected = collected;
-            this.flightID = flightID;
-            this.bookedSeats = bookedSeats;
+            this.TicketID = ticketID;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.HashedCardNumber = hashedCardNumber;
+            this.Collected = collected;
+            this.FlightID = flightID;
+            this.BookedSeats = bookedSeats;
         }
 
-        public int ticketID { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string hashedCardNumber { get; set; }
-        public bool collected { get; set; }
-        public int flightID { get; set; }
-        public int bookedSeats { get; set; }
+        public int TicketID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HashedCardNumber { get; set; }
+        public bool Collected { get; set; }
+        public int FlightID { get; set; }
+        public int BookedSeats { get; set; }
 
     }
 }
